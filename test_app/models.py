a4178019dc3d0453e86b86ab97f042c5ec8a52d6
Ruslan <mellow.neliph@gmail.com>
2026-05-07 12:37:44 +0200

    added model Book

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100) # VarChar(255)
+    description = models.TextField()
+    price = models.FloatField()
+
+    published_date = models.DateField() # Datetime(%Y%m%d)
+
+
+"""
+CREATE TABLE IF NOT EXISTS 'test_app_book' (
+    id ...
+    title VarChar(100) NOT NULL
+    desctiption TEXT NOT NULL
+    published date DATE 
+)
+"""
