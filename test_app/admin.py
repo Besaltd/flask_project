9e6682f0f96229b36996d42dc07e07378264e806
Ruslan <mellow.neliph@gmail.com>
2026-06-02 21:55:53 +0200

    Add subtask inline, short title display, and mark done action in admin

--- test_app/admin.py
+++ test_app/admin.py
@@ -17,12 +17,15 @@ class SubTaskInline(admin.TabularInline):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "status", "deadline", "created_at")
+    list_display = ("id", "short_title", "status", "deadline", "created_at")
     list_filter = ("status", "categories")
     search_fields = ("title", "description")
     filter_horizontal = ("categories",)
     inlines = [SubTaskInline]
 
+    def short_title(self, obj):
+        return obj.title[:10] + "..." if len(obj.title) > 10 else obj.title
+    short_title.short_description = "Title"
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
@@ -30,3 +33,9 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_filter = ("status", "task")
     search_fields = ("title", "description")
 
+    def mark_done(modeladmin, request, queryset):
+        queryset.update(status=SubTask.Status.DONE)
+
+    mark_done.short_description = "Mark selected as Done"
+
+    actions = [mark_done]
